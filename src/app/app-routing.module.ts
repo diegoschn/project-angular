1f411ae990a0809b0af4e23b235d0e3e6db24902
Diego Schneider <dlcschneider1@gmail.com>
2023-11-28 12:31:54 -0300

    config routes to renderize components in app componnet root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', //se for caminho vazio.. ex: localhost:4200
+    redirectTo: 'listarPensamento', //redireciona para
+    pathMatch: 'full'//ele pega a url completa
+  },
+  {
+    path: 'criarPensamento', //ex: localhost:4200/criarPensamento
+    component: CriarPensamentoComponent //renderizar componente 
+  },
+  {
+    path: 'listarPensamento', //ex: localhost:4200/listarPensamento
+    component: ListarPensamentoComponent //renderizar componente 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
