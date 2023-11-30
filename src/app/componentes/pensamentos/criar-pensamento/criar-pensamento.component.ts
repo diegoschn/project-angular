e1962d03b49815af30a6c9edb4ad2e8a90a35c11
Diego Schneider <dlcschneider1@gmail.com>
2023-11-30 19:08:59 -0300

    applying more than 1 validation in reactive form

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -23,14 +23,21 @@ export class CriarPensamentoComponent implements OnInit {
   ngOnInit(): void {
     //iniciando objeto que será linkado ao template
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]], 
-      autoria: ['', [Validators.required]],
+      //Validators.compose é usado quando se quer fazer mais de 1 validação
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],  
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+      ])],    
       modelo: ['modelo1']
     })
   }
 
   criarPensamento(){
-    console.log(this.formulario)
+    console.log(this.formulario.status)
     if(this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(()=>{
         this.router.navigate(['/listarPensamento'])
